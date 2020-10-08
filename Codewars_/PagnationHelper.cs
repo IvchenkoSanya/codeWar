73712f455684768c32a87f29d102d1cc14be5d5d
Ivchenko Aleksander <ivchenko.aleksander@prime-group.com.ua>
2020-10-08 12:00:15 +0300

    PaginationHelper

---
+++ Codewars_/PagnationHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codewars_
+{
+    
+    class PagnationHelper<T>
+    {
+        IList<T> _collection;
+        int _itemsPerPage;
+
+        public PagnationHelper(IList<T> collection, int itemsPerPage)
+        {
+            this._collection = collection;
+            this._itemsPerPage = itemsPerPage;
+        }
+
+        public int ItemCount
+        {
+            get
+            {
+                return _collection.Count;
+            }
+        }
+        public int PageCount
+        {
+            get
+            {
+                if (ItemCount % _itemsPerPage != 0)
+                    return (ItemCount / _itemsPerPage) + 1;
+                return ItemCount / _itemsPerPage;
+            }
+        }
+        public int PageItemCount(int pageIndex)
+        {
+            if (pageIndex < 0 || pageIndex >= PageCount)
+                return -1;
+
+            if (pageIndex < PageCount - 1)
+                return _itemsPerPage;
+            else
+                return ItemCount % _itemsPerPage;
+        }
+        public int PageIndex(int itemIndex)
+        {
+            if (itemIndex < 0 || itemIndex >= ItemCount)
+                return -1;
+
+            return itemIndex / _itemsPerPage;
+        }
+    }
+}
---
+++ Codewars_/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codewars_
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            IList<int> collection = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
+
+            var helper = new PagnationHelper<int>(collection, 10);
+            Console.WriteLine(helper.PageIndex(10));
+            Console.WriteLine(helper.PageIndex(24));
+
+            Console.WriteLine();
+
+
+            // pageIndex takes an item index and returns the page that it belongs on
+            //should == 1 (zero based index)
+            //should == 0
+             //should == -1
+            
+
+            Console.ReadKey();
+        }
+    }
+}
