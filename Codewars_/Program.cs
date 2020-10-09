474824f8a51aefd9393d3194bd9b1e986873edb0
Ivchenko Aleksander <ivchenko.aleksander@prime-group.com.ua>
2020-10-09 09:22:17 +0300

    Added SimplePigLatin solution

--- Codewars_/Program.cs
+++ Codewars_/Program.cs
@@ -8,21 +8,11 @@ namespace Codewars_
         
         static void Main(string[] args)
         {
-            IList<int> collection = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
-
-            var helper = new PagnationHelper<int>(collection, 10);
-            Console.WriteLine(helper.PageIndex(10));
-            Console.WriteLine(helper.PageIndex(24));
-
-            Console.WriteLine();
-
-
-            // pageIndex takes an item index and returns the page that it belongs on
-            //should == 1 (zero based index)
-            //should == 0
-             //should == -1
+           var str1 =  Kata.PigIt("Pig latin is cool");
+            var str2 = Kata.PigIt("Hello world !");
+                Console.WriteLine(str1);
+                Console.WriteLine(str2);
             
-
             Console.ReadKey();
         }
     }
---
+++ Codewars_/SimplePigLatin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Codewars_
+{
+    public class Kata
+    {
+        public static string PigIt(string str)
+        {
+            #region firstAttemp
+
+            var resStr = "";
+
+            foreach (string item in str.Split(' '))
+            {
+                if (item.Length > 1)
+                    resStr += item.Remove(0, 1) + item.Substring(0, 1) + "ay ";
+                else
+                    resStr += item + " ";
+            }
+            return resStr.Remove(resStr.Length-1);
+
+            #endregion
+            //return string.Join(" ", str.Split(' ').Select(x => x.Substring(1) + x.Substring(0, 1) + "ay"));
+        }
+    }
+}
