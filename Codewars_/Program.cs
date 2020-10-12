79266bb82bf064b281405c222408bfd93a489a67
Ivchenko Aleksander <ivchenko.aleksander@prime-group.com.ua>
2020-10-12 10:47:19 +0300

    Added SortBinaryTree solution

--- Codewars_/Program.cs
+++ Codewars_/Program.cs
@@ -8,9 +8,18 @@ namespace Codewars_
         
         static void Main(string[] args)
         {
-            Console.WriteLine(ROT13.Rot13("EBG13 rknzcyr."));
-            Console.WriteLine(ROT13.Rot13("This is my first ROT13 excercise!"));
-       
+            var x = SortBinaryTreeByLevels.TreeByLevels(null);
+
+            var y = SortBinaryTreeByLevels.TreeByLevels(new Node(new Node(null, new Node(null, null, 4), 2), new Node(new Node(null, null, 5), new Node(null, null, 6), 3), 1));
+
+            foreach (var item in y)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
 
                         Console.ReadKey();
         }
---
+++ Codewars_/SortBinaryTreeByLevels.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codewars_
+{
+    public class Node
+    {
+        public Node Left;
+        public Node Right;
+        public int Value;
+
+        public Node(Node l, Node r, int v)
+        {
+            Left = l;
+            Right = r;
+            Value = v;
+        }
+    }
+    class SortBinaryTreeByLevels
+    {
+        public static void TreeByLevels(Node node, ref List<int> resList)
+        {
+            var queue = new Queue<Node>();
+            resList.Add(node.Value);
+            queue.Enqueue(node);
+            while (queue.Count != 0)
+            {
+                if (queue.Peek().Left != null)
+                {
+                    resList.Add(queue.Peek().Left.Value);
+                    queue.Enqueue(queue.Peek().Left);
+                }
+                if (queue.Peek().Right != null)
+                {
+                    resList.Add(queue.Peek().Right.Value);
+                    queue.Enqueue(queue.Peek().Right);
+                }
+                queue.Dequeue();
+            }
+        }
+        public static List<int> TreeByLevels(Node node)
+        {
+            if (node == null)
+                return new List<int>();
+
+            List<int> resList = new List<int>();
+
+            TreeByLevels(node, ref resList);
+
+            return resList;
+        }
+
+
+
+    }
+}
